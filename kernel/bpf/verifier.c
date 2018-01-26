7257e4a64e98d786a2781ef9a1280aac4a47a9c1
Artem Labazov <123321artyom@gmail.com>
2018-01-26 17:58:08 +0300

    kernel
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -315,7 +315,7 @@ static const char *const bpf_jmp_string[] = {
 	[BPF_EXIT >> 4] = "exit",
 };
 
-static void print_bpf_insn(const struct verifier_env *env,
+static void print_bpf_insn(const struct bpf_verifier_env *env,
 			   const struct bpf_insn *insn)
 {
 	u8 class = BPF_CLASS(insn->code);
